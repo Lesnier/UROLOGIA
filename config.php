5e776abaf124a7d5e1a8577f8e4dfe1cb1d0c891
Lesnier <lesniergonzalez@gmail.com>
2017-01-06 10:54:02 -0500

    Se cambio el echo
    Se da funcion echo como resultado

--- config.php
+++ config.php
@@ -8,3 +8,5 @@ $numero2 = 20;
 $resultSuma = $numero1 + $numero2;
 $resultDiv = $numero1 / $numero2;
 
+echo "Este es el resultado de la divicion ".$resultDiv;
+echo "Este es el resultado  de la suma".$resultSuma;
